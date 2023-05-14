7b2eb1ce86ca15e32d043bcec7a1c9e028d9270d
Andre Dupczak <andre.dupczak@kellogg.northwestern.edu>
2023-05-14 23:50:08 +0000

    Second commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,16 +8,16 @@ class PostsController < ApplicationController
     end
 
   def new
-    @posts = Post.new
-    @posts["place_id"] = params["place_id"]
+    @post = Post.new
+    @post["place_id"] = params["place_id"]
   end
   
   def create
     @posts = Post.new
-    puts params ["post"]
+    puts params["post"]
     @posts["title"] = params["post"]["title"]
     @posts["description"] = params["post"]["description"]
-    @posts["place_id"] = params["posts"]["place_id"]
+    @posts["place_id"] = params["post"]["place_id"]
     @posts.save
     redirect_to "/places/#{@posts["place_id"]}"
   end
